f10ab525122523e33811157af0a9701651dd014c
vidh2000 <vid.homsak19@imperial.ac.uk>
2023-09-17 13:51:29 +0200

    should work? Needs testing

--- model.py
+++ model.py
@@ -1,67 +1,64 @@
 import torch
-import os 
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+import torch.nn.functional as F
+import os
 
 class Linear_QNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self,input_size,hidden_size,output_size):
         super().__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
- 
+        self.linear1 = nn.Linear(input_size,hidden_size).cuda()
+        self.linear2 = nn.Linear(hidden_size,output_size).cuda()
+    
     def forward(self, x):
         x = F.relu(self.linear1(x))
         x = self.linear2(x)
         return x
- 
-    def save(self, file_name='model_name.pth'):
-        model_folder_path = 'Path'
-        file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict(), file_name)
+    
+    def save(self, file_name='model.pth'):
+        model_folder_path = r'C:\Users\Asus\Documents\Coding\Python\Machine Learning\SnakeGameRL' 
+        file_name = os.path.join(model_folder_path,file_name)
+        torch.save(self.state_dict(),file_name)
 
 class QTrainer:
     def __init__(self,model,lr,gamma):
-        #Learning Rate for Optimizer
         self.lr = lr
-        #Discount Rate
         self.gamma = gamma
-        #Linear NN defined above.
         self.model = model
-        #optimizer for weight and biases updation
-        self.optimer = optim.Adam(model.parameters(),lr = self.lr)
-        #Mean Squared error loss function
+        self.optimer = optim.Adam(model.parameters(),lr = self.lr)    
         self.criterion = nn.MSELoss()
- 
-     
-    def train_step(self, state, action, reward, next_state, done):
-        state = torch.tensor(state,dtype=torch.float)
-        next_state = torch.tensor(next_state,dtype=torch.float)
-        action = torch.tensor(action,dtype=torch.long)
-        reward = torch.tensor(reward,dtype=torch.float)
- 
-        #if only one parameter to train , then convert to tuple of shape (1, x)
-        if(len(state.shape) == 1):
-            #(1, x)
-            state = torch.unsqueeze(state,0)
-            next_state = torch.unsqueeze(next_state,0)
-            action = torch.unsqueeze(action,0)
-            reward = torch.unsqueeze(reward,0)
+        for i in self.model.parameters():
+            print(i.is_cuda)
+
+    
+    def train_step(self,state,action,reward,next_state,done):
+        state = torch.tensor(state,dtype=torch.float).cuda()
+        next_state = torch.tensor(next_state,dtype=torch.float).cuda()
+        action = torch.tensor(action,dtype=torch.long).cuda()
+        reward = torch.tensor(reward,dtype=torch.float).cuda()
+
+        if(len(state.shape) == 1): # only one parameter to train , Hence convert to tuple of shape (1, x)
+            #(1 , x)
+            state = torch.unsqueeze(state,0).cuda()
+            next_state = torch.unsqueeze(next_state,0).cuda()
+            action = torch.unsqueeze(action,0).cuda()
+            reward = torch.unsqueeze(reward,0).cuda()
             done = (done, )
- 
+
         # 1. Predicted Q value with current state
-        pred = self.model(state)
-        target = pred.clone()
+        pred = self.model(state).cuda()
+        target = pred.clone().cuda()
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx]+self.gamma*torch.max(self.model(next_state[idx]))
-            target[idx][torch.argmax(action).item()] = Q_new
-        # 2. Q_new = reward + gamma * max(next_predicted Qvalue)
-        #pred.clone()
-        #preds[argmax(action)] = Q_new
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx])).cuda()
+            target[idx][torch.argmax(action).item()] = Q_new 
+        # 2. Q_new = reward + gamma * max(next_predicted Qvalue) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
         self.optimer.zero_grad()
         loss = self.criterion(target,pred)
-        loss.backward() # backward propagation of loss
- 
+        loss.backward()
+
         self.optimer.step()
+  
---
+++ test.py
@@ -0,0 +1,4 @@
+a = [1,2,3,4]
+d = 3
+b = a.index(d)
+print(b)
